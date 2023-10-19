99c776dd13586faaa928bc2655bb5917e3dd3d92
Muhamad Abdullah <abdulx125@gmail.com>
2023-10-18 18:43:58 -0700

    JS almost done

---
+++ script.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('button');
+const screenDisplay = document.querySelectorAll('.screen')
+// now we make a function that's calculate
+
+function calculate(button) {
+    console.log(button);
+}
+// now use foreach to get the buttons
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
