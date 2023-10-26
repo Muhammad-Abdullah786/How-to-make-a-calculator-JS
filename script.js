ea239a984c1ce7a2ca2c40295d8c443b4cdbfab5
Muhamad Abdullah <abdulx125@gmail.com>
2023-10-25 22:53:23 -0700

    alittle bit more correction

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ const buttons = document.querySelectorAll('button');
 const screenDisplay = document.querySelector('.screen')
 // now we make a function that's calculate
 let calculation = []// array to push all my value into
-let calculationWithoutChar ;
+let calculationWithoutChar;
 function calculate(button) {
     const value = button.textContent;
-    if (value ==="Clean") {
-        calculation  = []
-        screenDisplay.textContent  = '.'
-        
+    if (value === "Clean") {
+        calculation = []
+        screenDisplay.textContent = '.'
+
     }
-    else if (value === '='){
-        screenDisplay.textContent  = eval(calculationWithoutChar)
+    else if (value === '=') {
+        screenDisplay.textContent = eval(calculationWithoutChar)
     }
-    else{
-    calculation.push(value)
-    calculationWithoutChar = calculation.join('')
-    screenDisplay.textContent  = calculationWithoutChar
+    else {
+        calculation.push(value)
+        calculationWithoutChar = calculation.join('')
+        screenDisplay.textContent = calculationWithoutChar
     }
-    
+
 }
 // now use foreach to get the buttons
 
