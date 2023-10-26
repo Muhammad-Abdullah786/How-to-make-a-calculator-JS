4446a1c22b54df939d3fe3d50c8cf91d76378e82
Muhamad Abdullah <abdulx125@gmail.com>
2023-10-25 22:27:09 -0700

    learning eval

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const buttons = document.querySelectorAll('button');
-const screenDisplay = document.querySelectorAll('.screen')
+const screenDisplay = document.querySelector('.screen')
 // now we make a function that's calculate
-
+let calculation = []// array to push all my value into
+let calculationWithoutChar ;
 function calculate(button) {
-    console.log(button);
+    if (value ==="Clean") {
+        calculation  = []
+        screenDisplay.textContent  = '.'
+
+    }
+    else if (value === '='){
+        
+    }
+    let value = button.textContent;
+    calculation.push(value)
+    calculationWithoutChar = calculation.join('')
+    screenDisplay.textContent  = calculationWithoutChar
 }
 // now use foreach to get the buttons
 
