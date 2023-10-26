5ce604f9448eb8e39fb8aa0d7d9f6aa3bd0f3ca6
Muhamad Abdullah <abdulx125@gmail.com>
2023-10-26 00:03:47 -0700

    completed

--- script.js
+++ script.js
@@ -10,13 +10,40 @@ function calculate(button) {
         screenDisplay.textContent = '.'
 
     }
+    else if( value === 'del'){
+        calculation.pop();
+        calculationWithoutChar = calculation.join('')
+        screenDisplay.textContent = calculationWithoutChar
+    }
     else if (value === '=') {
         screenDisplay.textContent = eval(calculationWithoutChar)
     }
     else {
+         // Check for consecutive operators
+         const lastChar = calculation[calculation.length - 1];
+        
+         if (specialCharacterCheck(lastChar) && specialCharacterCheck(value)) {
+             // If both lastChar and value are operators, replace the last operator with the new one.
+             calculation.pop();
+         }
+ 
         calculation.push(value)
         calculationWithoutChar = calculation.join('')
         screenDisplay.textContent = calculationWithoutChar
+    }
+    // now what if the user add + character to the calculation 2 or more that 2 time for that we will use switch case and put it in the function
+    function specialCharacterCheck(character){
+        switch (character) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '.':
+                return true;
+            default:
+                return false;
+        }
+
     }
 
 }
